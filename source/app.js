aa421a614cfedbb4920f0479a89d1e05894baf1f
Your Name <santiago.perez777@gmail.com>
2022-08-25 13:52:38 +0200

    added functionalities

--- source/app.js
+++ source/app.js
@@ -3,13 +3,34 @@ const addToDoText = document.querySelector("#to-do-input");
 const toDoList = document.querySelector(".list");
 const toDoForm = document.querySelector("#to-do-form");
 
-toDoForm.addEventListener("submit", function addToList(e) {
-  e.preventDefault();
-  const newLi = document.createElement("Li");
-  newLi.innerText = addToDoText.value;
+function deleteToDo() {
+  newLi.remove();
+}
+
+function newButton() {
   const newButton = document.createElement("button");
   newButton.classList = "delete-button";
   newButton.innerHTML = "delete";
-  toDoList.append(newLi, newButton);
+  toDoList.append(newButton);
+  newButton.addEventListener("click", function () {
+    console.log("usuwam");
+    newLi.value = "";
+  });
+}
+function newLi() {
+  const newLi = document.createElement("Li");
+  newLi.innerText = addToDoText.value;
+  toDoList.append(newLi);
+  newButton();
   addToDoText.value = "";
+}
+
+toDoForm.addEventListener("submit", function addToList(e) {
+  e.preventDefault();
+  if (addToDoText.value === "") {
+    alert('please fill "to do"');
+    return;
+  } else {
+    newLi();
+  }
 });
